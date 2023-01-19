20c92059f4a7b2bdc9fed35db27c67688cba75a4
GDIINDIA\mansharma <mansharma@birchstreet.net>
2023-01-20 01:30:35 +0530

    last commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
  
 ReactDOM.render(
   <React.StrictMode>
